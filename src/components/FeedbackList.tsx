35488b8bd9f5fe6c53d9764af1b5276080c1f25a
Justin OConnell <itsjustinoconnell@gmail.com>
2024-01-25 11:24:09 +0000

    fetch status types created and loading spinner component added during fetch request

--- src/components/FeedbackList.tsx
+++ src/components/FeedbackList.tsx
@@ -1,25 +1,38 @@
 import { useEffect, useState } from "react";
 import FeedbackItem from "./FeedbackItem";
+import LoadingSpinner from "./LoadingSpinner";
 
 const API_URL =
   "https://bytegrad.com/course-assets/projects/corpcomment/api/feedbacks";
 
+type Status = "loading" | "complete";
+
 export default function FeedbackList() {
   const [userFeedback, setUserFeedback] = useState([]);
+  const [status, setStatus] = useState<Status>();
 
   useEffect(() => {
-    (async function getUserData() {
-      const resp = await fetch(API_URL);
-      const { feedbacks } = await resp.json();
-      setUserFeedback(feedbacks);
-    })();
+    setStatus("loading"),
+      (async function getUserData() {
+        const resp = await fetch(API_URL);
+        const { feedbacks } = await resp.json();
+        setUserFeedback(feedbacks);
+        setStatus("complete");
+      })();
   }, []);
 
   return (
     <ol className="feedback-list">
-      {userFeedback.map((feedbackItem) => (
+      {status === "loading" ? (
+        <LoadingSpinner />
+      ) : (
+        userFeedback.map((feedbackItem) => (
+          <FeedbackItem feedbackItem={feedbackItem} />
+        ))
+      )}
+      {/* {userFeedback.map((feedbackItem) => (
         <FeedbackItem feedbackItem={feedbackItem} />
-      ))}
+      ))} */}
     </ol>
   );
 }
